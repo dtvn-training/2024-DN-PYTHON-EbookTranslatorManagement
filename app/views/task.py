ce326b609414487d1c090a6b9bf494a66aabb6ff
NguyentThanhAn <nguyenthanhanqt@gmail.com>
2024-12-09 12:03:30 +0700

    Implement API for view task Page

---
+++ app/views/task.py
@@ -0,0 +1,13 @@
+from app.controllers.task import get_tasks_controllers
+from flask import jsonify, Blueprint, request
+from datetime import datetime
+
+task = Blueprint('task', __name__, url_prefix="/task")
+
+
+@task.route("/")
+def get_tasks():
+    key = request.args.get("key", "")
+    deadline = request.args.get("deadline", None)
+    task_category_id = request.args.get("task_category_id", None)
+    return jsonify(get_tasks_controllers(key, deadline, task_category_id))
