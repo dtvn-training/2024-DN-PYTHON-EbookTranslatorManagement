fe7c84c1ff4383d146694d0bc4ab3ff206e3c4f6
NguyentThanhAn <nguyenthanhanqt@gmail.com>
2024-12-09 09:29:55 +0700

    Delete unnecessary code and assets

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from database.db import db
-from .controllers.userController import user as userController
 from utils.secret import db_url
 
 
@@ -8,7 +7,6 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     db.init_app(app)
-    app.register_blueprint(userController)
     with app.app_context():
         from .models import Level, Task, Chapter, User, TaskCategory, Book, Comment, Content, Notification, Profile, Role, KPI, UserNotification
         db.create_all()
