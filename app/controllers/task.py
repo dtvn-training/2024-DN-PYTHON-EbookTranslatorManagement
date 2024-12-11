9e03b0c4bd4aade47e399ffde0abec52cf0b5eb5
NguyentThanhAn <nguyenthanhanqt@gmail.com>
2024-12-11 10:29:05 +0700

    Create class to transform  data

--- app/controllers/task.py
+++ app/controllers/task.py
@@ -1,19 +1,14 @@
 from dateutil import parser
 from app.services.task import get_tasks
+from flask import request
 
 
 def get_tasks_controllers(key="", deadline=None, task_category_id=None):
+    key = request.args.get("key", "")
+    deadline = request.args.get("deadline", None)
+    task_category_id = request.args.get("task_category_id", None)
     if deadline:
         deadline = parser.parse(deadline)
         deadline = deadline.strftime("%Y-%m-%d %H:%M:%S")
     tasks = get_tasks(key, deadline, task_category_id)
-    tasks = [{
-        "task_id": task[0],
-        "chapter_title": task[1],
-        "deadline": task[2],
-        "author": task[3],
-        "book_title": task[4],
-        "type": task[5],
-        "language": task[6],
-    } for task in tasks]
     return tasks
---
+++ app/interfaces/Task.py
@@ -0,0 +1,29 @@
+class Task:
+    def __init__(self, task_id, chapter_title, deadline, type, language):
+        self.task_id = task_id
+        self.chapter_id = chapter_title
+        self.deadline = deadline
+        self.type = type
+        self.language = language
+
+
+class Task_Management(Task):
+    def __init__(self, task_id, chapter_title, deadline, type, language, author):
+        super().__init__(task_id, chapter_title, deadline, type, language)
+        self.author = author
+
+    def to_dict(self):
+        return {
+            "task_id": self.task_id,
+            "chapter_title": self.chapter_id,
+            "deadline": self.deadline,
+            "author": self.author,
+            "type": self.type,
+            "language": self.language
+        }
+
+    @classmethod
+    def create(cls, task):
+        task_dict = Task_Management(
+            task[0], task[1], task[2], task[3], task[4], task[5])
+        return task_dict
