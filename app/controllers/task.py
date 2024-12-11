68e4f06bc4c0fa24161aade70ebab12155b23510
NguyentThanhAn <nguyenthanhanqt@gmail.com>
2024-12-11 17:00:24 +0700

    implement_register_task

--- app/controllers/task.py
+++ app/controllers/task.py
@@ -1,6 +1,7 @@
 from dateutil import parser
-from app.services.task import get_tasks
+from app.services.task import get_tasks, get_register_tasks_service, register_task
 from flask import request
+from flask_jwt_extended import get_jwt_identity
 
 
 def get_tasks_controllers(key="", deadline=None, task_category_id=None):
@@ -15,3 +16,21 @@ def get_tasks_controllers(key="", deadline=None, task_category_id=None):
         deadline = deadline.strftime("%Y-%m-%d %H:%M:%S")
     tasks = get_tasks(key, deadline, task_category_id)
     return tasks
+
+
+def get_register_tasks_controller():
+    key = request.args.get("key", "")
+    type = request.args.get("type", "")
+    language = request.args.get("language", "")
+    tasks = get_register_tasks_service(key, type, language)
+    return tasks
+
+
+def registe_task_controller(task_id):
+    try:
+        id = int(task_id)
+        user = get_jwt_identity()
+        register = register_task(id, user.user_id)
+        print(register)
+    except:
+        return None
