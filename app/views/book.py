207c5f997c09530e4644e384e707d86a97c73ac9
NguyentThanhAn <nguyenthanhanqt@gmail.com>
2024-12-25 08:49:24 +0700

    handle status code for progress tracking

--- app/views/book.py
+++ app/views/book.py
@@ -4,7 +4,9 @@ from app.controllers.book import progress_tracking_controller
 books = Blueprint("books", __name__, url_prefix="/api/ebook")
 
 
-@books.route("/proress-tracking", methods=["GET"])
+@books.route("/progress-tracking", methods=["GET"])
 def progress_tracking():
-    # progress_tracking_controller()
-    return jsonify(progress_tracking_controller())
+    progress_tracking = progress_tracking_controller()
+    if progress_tracking["is_success"]:
+        return jsonify(progress_tracking), 200
+    return jsonify(progress_tracking), 400
