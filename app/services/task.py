ec733d742d19a82780f9e0cbd00f1c8ea4d7475c
Annn <Annndang@gmail.com>
2024-12-30 16:59:54 +0700

    implement be dashboard

--- app/services/task.py
+++ app/services/task.py
@@ -1,5 +1,6 @@
 from app.models import TaskCategory, Book, Profile, User, Chapter, Task
 from app.interfaces import Task_Management
+import datetime
 
 
 def get_tasks(key, deadline, task_category_id):
@@ -24,3 +25,46 @@ def get_tasks(key, deadline, task_category_id):
     tasks = tasks.all()
     tasks = [Task_Management.create(task).to_dict() for task in tasks]
     return tasks
+
+def count_total_task():
+    return Task.query.count()
+
+def count_task_current_month():
+    current_month = datetime.now().month
+    current_year = datetime.now().year
+    return Task.query.filter(
+        Task.deadline.isnot(None),
+        Task.deadline.month == current_month,
+        Task.deadline.year == current_year
+    ).count() 
+
+def count_completed_task():
+    return Task.query.filter(Task.is_completed == True).count()
+
+def count_uncompleted_task():
+    return Task.query.filter(Task.is_completed == False).count()
+
+def count_task_last_month():
+    last_month = datetime.now().month - 1 if datetime.now().month != 1 else 12
+    last_month_year = datetime.now().year if datetime.now().month != 1 else datetime.now().year - 1
+    return Task.query.filter(
+        Task.deadline.isnot(None),
+        Task.deadline.month == last_month,
+        Task.deadline.year == last_month_year
+    ).count()
+
+
+def count_task_each_day():
+    today = datetime.now()
+    start_of_month = datetime(today.year, today.month, 1)
+    
+    task_counts_per_day = {}
+    tasks = Task.query.filter(Task.deadline >= start_of_month).all()
+
+    for task in tasks:
+        task_day = task.deadline.date()
+        if task_day not in task_counts_per_day:
+            task_counts_per_day[task_day] = 0
+        task_counts_per_day[task_day] += 1
+
+    return task_counts_per_day
