cf4144c95f6f08e773d90c52e43707278a90d308
Annn <Annndang@gmail.com>
2024-12-31 11:28:45 +0700

    render data into  dashboard

--- app/services/task.py
+++ app/services/task.py
@@ -1,8 +1,7 @@
 from app.models import TaskCategory, Book, Profile, User, Chapter, Task
 from app.interfaces import Task_Management
+from database import db
 import datetime
-
-
 def get_tasks(key, deadline, task_category_id):
     tasks = Task.query.join(Chapter, Task.chapter_id == Chapter.chapter_id).join(
         User, Task.user_id == User.user_id
@@ -21,22 +20,14 @@ def get_tasks(key, deadline, task_category_id):
     if task_category_id:
         tasks = tasks.filter(Task.task_category_id == task_category_id)
     tasks = tasks.with_entities(
-        Task.task_id, Chapter.chapter_title, Task.deadline, TaskCategory.title, Book.language,Profile.fullname)
+        Task.task_id, Chapter.chapter_title, Task.deadline, TaskCategory.title, Book.language, Profile.fullname)
     tasks = tasks.all()
     tasks = [Task_Management.create(task).to_dict() for task in tasks]
     return tasks
 
+
 def count_total_task():
     return Task.query.count()
-
-def count_task_current_month():
-    current_month = datetime.now().month
-    current_year = datetime.now().year
-    return Task.query.filter(
-        Task.deadline.isnot(None),
-        Task.deadline.month == current_month,
-        Task.deadline.year == current_year
-    ).count() 
 
 def count_completed_task():
     return Task.query.filter(Task.is_completed == True).count()
@@ -44,27 +35,15 @@ def count_completed_task():
 def count_uncompleted_task():
     return Task.query.filter(Task.is_completed == False).count()
 
-def count_task_last_month():
-    last_month = datetime.now().month - 1 if datetime.now().month != 1 else 12
-    last_month_year = datetime.now().year if datetime.now().month != 1 else datetime.now().year - 1
-    return Task.query.filter(
-        Task.deadline.isnot(None),
-        Task.deadline.month == last_month,
-        Task.deadline.year == last_month_year
-    ).count()
+def get_task_summary():
+    total_task = Task.query.count()
+    completed_task = Task.query.filter(Task.is_completed == True).count()
+    uncompleted_task = Task.query.filter(Task.is_completed == False).count()
+
+    return {
+        "total_task": total_task,
+        "completed_task": completed_task,
+        "uncompleted_task": uncompleted_task
+    }
 
 
-def count_task_each_day():
-    today = datetime.now()
-    start_of_month = datetime(today.year, today.month, 1)
-    
-    task_counts_per_day = {}
-    tasks = Task.query.filter(Task.deadline >= start_of_month).all()
-
-    for task in tasks:
-        task_day = task.deadline.date()
-        if task_day not in task_counts_per_day:
-            task_counts_per_day[task_day] = 0
-        task_counts_per_day[task_day] += 1
-
-    return task_counts_per_day
--- app/views/task.py
+++ app/views/task.py
@@ -1,9 +1,32 @@
-from app.controllers.task import get_tasks_controllers
+from app.controllers.task import (
+    get_tasks_controllers,
+    count_completed_task_controllers,
+    count_total_task_controllers,
+    count_uncompleted_task_controllers,
+    get_task_summary
+)
 from flask import jsonify, Blueprint, request
 
 task = Blueprint('task', __name__, url_prefix="/api/task")
 
 
 @task.route("/")
-def get_tasks():
+def get_tasks_view():
     return jsonify(get_tasks_controllers())
+
+@task.route("/completed-task")
+def count_completed_task_view():
+    return jsonify(count_completed_task_controllers())
+
+@task.route("/uncompleted-task")
+def count_uncompleted_task_view():
+    return jsonify(count_uncompleted_task_controllers())
+
+@task.route("/total-task")
+def count_total_task_view():
+    return jsonify(count_total_task_controllers())
+
+@task.route("/dashboard")
+def get_data_to_dashboard():
+    data_dict = get_task_summary()
+    return jsonify(data_dict)
