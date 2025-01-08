402b0cc5a9ad6f163342c98416e07dde99864c08
Annn <Annndang@gmail.com>
2025-01-08 16:07:45 +0700

    complete PB03_Dashboard_be

--- app/controllers/task.py
+++ app/controllers/task.py
@@ -1,9 +1,20 @@
 from dateutil import parser
-from app.services.task import get_tasks, get_register_tasks_service, register_task_service
 from flask import request
 from flask_jwt_extended import get_jwt_identity
 from app.interfaces import Response
-
+import datetime
+from app.services.task import (
+    get_tasks,
+    get_total_task,
+    get_register_tasks_service,
+    register_task_service,
+    get_completed_task,
+    get_uncompleted_task,
+    get_task_per_month,
+    count_task_per_day,
+    count_task_summary,
+    get_tasks_to_table
+)
 
 def get_tasks_controllers(key="", deadline=None, task_category_id=None):
     key = request.args.get("key", "")
@@ -36,3 +47,33 @@ def registe_task_controller(task_id):
         return Response.create(True, "Task registered successfully", None)
     except Exception as e:
         return Response.create(False, "Fail for registration", None)
+
+def get_task_summary_controllers():
+    now = datetime.datetime.now()
+    current_month = now.month
+    current_year = now.year
+
+    total_task = get_total_task()
+    completed_task = get_completed_task()
+    uncompleted_task = get_uncompleted_task()
+
+    tasks_per_day = count_task_per_day(current_month, current_year)
+    tasks_per_day_current_month = get_task_per_month(current_month, current_year)
+    tasks_per_day_last_month = get_task_per_month(current_month - 1, current_year)
+    count_task_in_month = count_task_summary(current_month, current_year)
+
+    return {
+        "total_task": total_task,
+        "completed_task": completed_task,
+        "uncompleted_task": uncompleted_task,
+        "count_task_of_month": count_task_in_month,
+        "count_task_per_day": tasks_per_day,
+        "tasks_per_day_current_month": tasks_per_day_current_month,
+        "tasks_per_day_last_month": tasks_per_day_last_month
+    }
+
+def get_tasks_to_table_controllers():
+    tasks = get_tasks_to_table()
+    if not tasks:
+        return []
+    return tasks
--- app/models/task.py
+++ app/models/task.py
@@ -10,7 +10,6 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     chapter_id = db.Column(db.Integer, db.ForeignKey(
         'chapter.chapter_id', ondelete='CASCADE'), nullable=False)
-    task_name = db.Column(db.String(100), nullable=False)
     deadline = db.Column(db.DateTime, nullable=False)
     salary = db.Column(db.Numeric(10, 2), default=Decimal('0.00'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
@@ -18,30 +17,30 @@ class Task(db.Model):
     task_category_id = db.Column(
         db.Integer, db.ForeignKey('task_category.task_category_id'))
     salary = db.Column(db.Numeric(10, 2), default=Decimal('0.00'))
+    base_salary_multiplier = db.Column(
+        db.Numeric(10, 2), default=Decimal('0.00'))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     deleted_at = db.Column(db.DateTime)
 
-    # Relationship với bảng Chapter, User và TaskCategory
     chapter = db.relationship('Chapter', backref='tasks', lazy=True)
     user = db.relationship('User', backref='tasks', lazy=True)
     task_category = db.relationship('TaskCategory', backref='tasks', lazy=True)
 
-    def __init__(self, chapter_id, task_name, deadline, user_id=None, task_category_id=None, is_completed=False, salary=0):
+    def __init__(self, chapter_id, deadline, user_id=None, task_category_id=None, is_completed=False, salary=0, base_salary_multiplier=0):
         self.chapter_id = chapter_id
-        self.task_name = task_name
         self.deadline = deadline
         self.salary = salary
         self.task_category_id = task_category_id
         self.is_completed = is_completed
         self.salary = salary
+        self.base_salary_multiplier = base_salary_multiplier
 
     def to_dict(self):
         return {
             "task_id": self.task_id,
             "chapter_id": self.chapter_id,
-            "task_name": self.task_name,
             "deadline": self.deadline,
             "salary": self.salary,
             "task_category_id": self.task_category_id,
@@ -49,4 +48,5 @@ class Task(db.Model):
             "created_at": self.created_at,
             "updated_at": self.updated_at,
             "deleted_at": self.deleted_at,
+            "base_salary_multiplier": self.base_salary_multiplier
         }
