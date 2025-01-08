907e59ac39dc57c76990ebbaf622d966f2778319
Annn <Annndang@gmail.com>
2025-01-08 16:07:45 +0700

    complete PB03_Dashboard_be

--- app/views/task.py
+++ app/views/task.py
@@ -1,6 +1,12 @@
-from app.controllers.task import get_tasks_controllers, get_register_tasks_controller, registe_task_controller
 from flask import jsonify, Blueprint, request
 from flask_jwt_extended import jwt_required
+from app.controllers.task import (
+    get_register_tasks_controller,
+    registe_task_controller,
+    get_tasks_controllers,
+    get_task_summary_controllers,
+    get_tasks_to_table_controllers
+)
 
 task = Blueprint('task', __name__, url_prefix="/api/task")
 
@@ -23,3 +29,15 @@ def register_task_by_id(task_id):
     if task["success"]:
         return jsonify(task), 201
     return jsonify(task), 400
+
+#Overview Data
+@task.route("/dashboard")
+def get_data_to_dashboard():
+    data_dict = get_task_summary_controllers()
+    return jsonify(data_dict)
+
+#Table Data
+@task.route("/table_tasks")
+def get_table_tasks():
+    print(get_tasks_to_table_controllers)
+    return get_tasks_to_table_controllers()
